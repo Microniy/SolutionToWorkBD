6461e1c079e6c3d13a1553eb0ffbabe4f1b86b58
Тихинький Алексей <microniy@mail.ru>
2020-02-17 19:14:57 +0300

    Added folder Resourses and first language template

--- ConfigSolution/App.xaml.cs
+++ ConfigSolution/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Globalization;
 
 namespace ConfigSolution
 {
@@ -13,5 +14,15 @@ namespace ConfigSolution
     /// </summary>
     public partial class App : Application
     {
+        private static readonly IList<CultureInfo> m_Languages = new List<CultureInfo>();
+        public static IList<CultureInfo> Languages => m_Languages;
+        public App()
+        {
+            m_Languages.Clear();
+            m_Languages.Add(new CultureInfo("ru-RU"));
+            //Default culture Russia if you need new culture add dictionary and items this
+        }
+        public static event EventHandler LanguageChanged;
+
     }
 }
