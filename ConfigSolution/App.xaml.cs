3256ed16bb056e13c9955b67f52f9a1327d8a481
Alex Tikhinkiy <microniy@mail.ru>
2020-02-18 08:02:51 +0300

    Added property Language and event "culture change"

--- ConfigSolution/App.xaml.cs
+++ ConfigSolution/App.xaml.cs
@@ -23,6 +23,51 @@ namespace ConfigSolution
             //Default culture Russia if you need new culture add dictionary and items this
         }
         public static event EventHandler LanguageChanged;
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value == null) throw new ArgumentNullException("value");
+                if (value == System.Threading.Thread.CurrentThread.CurrentUICulture) return;
+
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+
+                ResourceDictionary dict = new ResourceDictionary();
+                switch (value.Name)
+                {
+                    /* this template for input new culture. Dictionary new language to need name for example: lang.en-US.xaml
+                    case "en-US":
+                        dict.Source = new Uri(String.Format("Resources/lang.{0}.xaml", value.Name), UriKind.Relative);
+                        break;
+                        */
+                    default:
+                        dict.Source = new Uri("Resources/lang.xaml", UriKind.Relative);
+                        break;
+                }
+                // find current dictionary
+                ResourceDictionary oldDict = (from d in Application.Current.Resources.MergedDictionaries
+                                              where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang.")
+                                              select d).First();
+                // change dictionary
+                if (oldDict != null)
+                {
+                    int ind = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Insert(ind, dict);
+
+                }
+                else
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                }
+
+                LanguageChanged(Application.Current, new EventArgs());
+            }
+        }
 
     }
 }
