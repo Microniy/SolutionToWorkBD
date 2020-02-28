918db6f6ab6a9bccf34d2d1a3d72542e3f43b3e4
Alex Tikhinkiy <microniy@mail.ru>
2020-02-28 15:57:27 +0300

    Edited to save class with parameters. Added animation to xaml form

--- ConfigSolution/ConfigurationDataClass.cs
+++ ConfigSolution/ConfigurationDataClass.cs
@@ -7,10 +7,25 @@ using System.Threading.Tasks;
 namespace ConfigSolution
 {
     public class ConfigurationDataClass
-    {
-        public string NameServer { get; set; }
-        public string NameBase { get; set; }
-        public string Login { get; set; }
+    {       
+        private string _nameServer;
+        public string NameServer { get => _nameServer; set => _nameServer = value; }       
+        public void SetNameServer(string name)
+        {
+            _nameServer = name;
+        }
+        private string _nameBase;
+        public string NameBase { get => _nameBase; set => _nameBase = value; }
+        public void SetNameBase(string name)
+        {
+            _nameBase = name;
+        }
+        private string _login;
+        public string Login { get => _login; set => _login = value; }
+        public void SetLogin(string name)
+        {
+            _login = name;
+        }
         public ConfigurationDataClass()
         {
             NameServer = string.Empty;
--- ConfigSolution/MainWindow.xaml.cs
+++ ConfigSolution/MainWindow.xaml.cs
@@ -23,36 +23,37 @@ namespace ConfigSolution
         readonly CommandBinding bind_Open = new CommandBinding(ApplicationCommands.Open);
         private const string CRYPTED_KEY = "KeyCripted";//for example key crypted
         private Keeper keeper;
-        private ConfigurationDataClass ParamServer;
+        private ConfigurationDataClass ParamServer = new ConfigurationDataClass();
+        private delegate void SetMetod(string value);
         public MainWindow()
         {
             InitializeComponent();
             bind_Open.Executed += Bind_Open_Executed;
             this.CommandBindings.Add(bind_Open);
             keeper = Keeper.Instance(Keepers.Reg);
+            GetParamServer();
+            this.DataContext = ParamServer;
 
         }
-        private void GetParamServer()
+        private void GetParamServer() //get save parameters to binding
         {
-            /*string param1 = keeper.GetParam("Parameter_1");
-            if (string.IsNullOrEmpty(param1))
+             
+            void GetParam(string paramName, SetMetod objParam)
             {
-                ParamServer.NameServer = string.Empty;
+                string param1 = keeper.GetParam(paramName);
+                if (string.IsNullOrEmpty(param1))
+                {
+                    objParam(string.Empty);
+                }
+                else
+                {
+                    objParam (StringCipher.Decrypt(param1, CRYPTED_KEY));
+                }
             }
-            else
-            {
-                ParamServer.NameServer = StringCipher.Decrypt(param1, CRYPTED_KEY);
-            }
-            */ //need to be method
-            string param2 = keeper.GetParam("Parameter_2");
-            if (string.IsNullOrEmpty(param2))
-            {
-                ParamServer.NameBase = string.Empty;
-            }
-            else
-            {
-                ParamServer.NameBase = StringCipher.Decrypt(param2, CRYPTED_KEY);
-            }
+
+            GetParam("Parameter_1", ParamServer.SetNameServer);
+            GetParam("Parameter_2", ParamServer.SetNameBase);
+            GetParam("Parameter_3", ParamServer.SetLogin);
         }
 
         private void Bind_Open_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -70,6 +71,29 @@ namespace ConfigSolution
                 MessageBox.Show(err.StackTrace);
             }
 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            System.Windows.Media.Animation.DoubleAnimation showAnimation = new System.Windows.Media.Animation.DoubleAnimation();
+            showAnimation.From = stackButtons.ActualHeight;
+            showAnimation.To = 30;
+            showAnimation.Duration = TimeSpan.FromSeconds(1);
+            stackButtons.BeginAnimation(StackPanel.HeightProperty, showAnimation);
+        }
+
+        private void TextBox_TextChanged(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Media.Animation.DoubleAnimation showAnimation = new System.Windows.Media.Animation.DoubleAnimation();
+            showAnimation.From = stackButtons.ActualHeight;
+            showAnimation.To = 30;
+            showAnimation.Duration = TimeSpan.FromSeconds(1);
+            stackButtons.BeginAnimation(StackPanel.HeightProperty, showAnimation);
         }
     }
 }
